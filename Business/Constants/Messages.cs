40a10b2b4cce7a1c241bab5b67558bc8694aff47
Alper Yurtdaş <73263073+Alperyrtds@users.noreply.github.com>
2021-05-14 21:03:56 +0300

    Add project files.

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,29 @@
+﻿using Core.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public class Messages
+    {
+        public static string ProductAdded = "Ürün eklendi";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz";
+        public static string MaintenanceTime = "Sistem Bakımda lütfen daha sonra tekrar deneyiniz";
+        public static string ProductsListed = "Ürünler listelendi.";
+        public static string UnitPriceInvalid = "Ürün Fiyatı geçersiz.";
+        public static string ProductCountOfCategoryError = "Stokta aynı üründen en fazla 10 tane olabilir.";
+        public static string ProductNameAlreadyExists = "Bu isimde başka bir ürün var";
+        public static string CategoryLimitExceded = "Kategori limiti aşıldığı için yeni ürün eklenemiyor.";
+        public static string AuthorizationDenied = "Yetkisiz giriş";
+        public static string UserRegistered = "Üye olundu";
+        public static string UserNotFound = "Kullanıcı Bulunamadı.";
+        public static string PasswordError = "Hatalı şifre";
+        public static string SuccessfulLogin = "Giriş başarılı";
+        public static string UserAlreadyExists = "Kullanıcı adı var";
+        public static string AccessTokenCreated;
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,33 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfProductDal : EfEntityRepositoryBase<Product, NorthWindContext>, IProductDal
+    {
+        public List<ProductDetailDTO> GetProductDetails()
+        {
+            using (NorthWindContext context = new NorthWindContext())
+            {
+                var result = from p in context.Products
+                             join c in context.Categories on p.CategoryId equals c.CategoryId
+                             select new ProductDetailDTO
+                             {
+                                 ProductId = p.ProductId,
+                                 ProductName = p.ProductName,
+                                 CategoryName = c.CategoryName,
+                                 UnitsInStock = p.UnitsInStock
+
+                             };
+                return result.ToList();
+            }
+        }
+    }
+}
